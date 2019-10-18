482969d7d7ee726550300bfbe363cf3dd221ad6b
Opokin D.S <mamhigt@mail.ru>
2019-10-19 01:56:46 +0700

    add arg parsing and crash dump printing barebones

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <argp.h>
 #include "parser/parser.h"
 
+
 int main(int argc, char *argv[]) {
+   error_t error = argp_parse(0, argc, argv, 0, 0, 0);
+   strerror(error);
    parse("enum tested { ADADAD, ADADAD, }");
 
    return EXIT_SUCCESS;
