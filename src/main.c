adc9ca847c4e56fa5230e53d626701701e95c1d8
Dmitry <mamhigt@mail.ru>
2019-11-07 00:32:49 +0700

    root: comment cli, only dump work

--- src/main.c
+++ src/main.c
@@ -3,20 +3,23 @@
 #include "parser/parser.h"
 #include <string.h>
 #include "cli.h"
+#include "ast/ast_tree.h"
+#include "ast/ast_dump.h"
 
 
 int main(int argc, char *argv[]) {
-    struct arguments arguments;
-    error_t error = argp_parse(&argp, argc, argv, 0, 0, &arguments);
-    strerror(error);
-    printf ("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
-            "VERBOSE = %s\nSILENT = %s\n",
-            arguments.args[0], arguments.args[1],
-            arguments.output_file,
-            arguments.verbose ? "yes" : "no",
-            arguments.silent ? "yes" : "no");
+//    struct arguments arguments;
+//    error_t error = argp_parse(&argp, argc, argv, 0, 0, &arguments);
+//    strerror(error);
+//    printf ("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
+//            "VERBOSE = %s\nSILENT = %s\n",
+//            arguments.args[0], arguments.args[1],
+//            arguments.output_file,
+//            arguments.verbose ? "yes" : "no",
+//            arguments.silent ? "yes" : "no");
 
-//    parse("enum tested { ADADAD, ADADAD, }");
+    struct translation_unit_t* tu = parse("_Static_assert(,);");
+    ast_dump(tu, stdin);
 
     return EXIT_SUCCESS;
 }
