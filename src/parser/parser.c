08db11a8918db0bbc76deae087bc33b51303beff
Temirkhan Myrzamadi <gymmasssorla@gmail.com>
2019-11-07 04:14:43 +0600

    Use the standard string functions (parser.c)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <setjmp.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/param.h>
@@ -32,16 +33,6 @@ typedef struct {
     jmp_buf checkpoint;
 } parser_t;
 
-bool memcpr(const void* src1, size_t size, const void* src2) {
-    for(size_t i = 0; i < size; ++i){
-        if(((u_int8_t*)src1)[i] != ((u_int8_t*)src2)[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 //TODO: bor
 
 typedef struct {
@@ -55,11 +46,9 @@ typedef struct {
     char* body;
 } comment_t;
 
-typedef struct {
-    size_t size;
-    char* body;
-} pstring_t;
-
+static bool is_starts_with(const char * restrict string, const char *restrict prefix) {
+    return strncmp(string, prefix, strlen(prefix)) == 0;
+}
 
 void skip_space(const char **str) {
     while((**str) == ' ' || (**str) == '\n'){
@@ -68,25 +57,26 @@ void skip_space(const char **str) {
 }
 
 bool is_assignment_operator(const char* str) {
-    const pstring_t patterns[] = {
-        (pstring_t){2, (char[]){'*','='}},
-        (pstring_t){2, (char[]){'/','='}},
-        (pstring_t){2, (char[]){'%','='}},
-        (pstring_t){2, (char[]){'+','='}},
-        (pstring_t){2, (char[]){'-','='}},
-        (pstring_t){3, (char[]){'<','<','='}},
-        (pstring_t){3, (char[]){'>','>','='}},
-        (pstring_t){2, (char[]){'&','='}},
-        (pstring_t){2, (char[]){'^','='}},
-        (pstring_t){2, (char[]){'|','='}}
+    const char * const patterns[] = {
+        "*=",
+        "/=",
+        "%=",
+        "+=",
+        "-=",
+        "<<=",
+        ">>=",
+        "&=",
+        "^=",
+        "|=",
     };
 
-    bool flag = false;
-    for(const pstring_t* p = patterns; p < patterns+(sizeof(patterns)/sizeof(pstring_t)+1); ++p){
-        flag = memcpr(str, p->size, p->body);
+    for(int i = 0; i < sizeof(patterns) / sizeof(patterns[0]); i++){
+        if (is_starts_with(str, patterns[i])) {
+            return true;
+        }
     }
 
-    return flag;
+    return false;
 }
 
 bool is_type_specifier() {
@@ -105,28 +95,29 @@ bool is_type_specifier() {
 }
 
 bool is_unary_expression(const char* str) {
-    const pstring_t patterns[] = {
-            (pstring_t) {2, (char[]) {'+', '+'}},
-            (pstring_t) {2, (char[]) {'-', '-'}},
+    const char *const  patterns[] = {
+            "++",
+            "--",
     };
 }
 
 bool is_unary_operator(const char* str) {
-    const pstring_t patterns[] = {
-            (pstring_t){1, (char[]){'&'}},
-            (pstring_t){1, (char[]){'*'}},
-            (pstring_t){1, (char[]){'+'}},
-            (pstring_t){1, (char[]){'-'}},
-            (pstring_t){1, (char[]){'~'}},
-            (pstring_t){1, (char[]){'!'}}
+    const char patterns[] = {
+            '&',
+            '*',
+            '+',
+            '-',
+            '~',
+            '!',
     };
 
-    bool flag = false;
-    for(const pstring_t* p = patterns; p < patterns+(sizeof(patterns)/sizeof(pstring_t)+1); ++p){
-        flag = memcpr(str, p->size, p->body);
+    for(int i = 0; i < sizeof(patterns) / sizeof(patterns[0]); i++) {
+        if (str[0] == patterns[i]) {
+            return true;
+        }
     }
 
-    return flag;
+    return false;
 }
 
 bool is_primary_expression() {
@@ -147,12 +138,8 @@ bool is_constant_expression() {
 }
 
 bool is_alignment_specifier(const char* str) {
-    pstring_t a = {8, (char[]){'_','A' ,'l','i','g','n','a','s'}};
-
-    bool flag1 = memcpr(str, a.size, a.body);
+    bool flag = strcmp(str, "_Alignas");
     bool type_name = is_type_name();
-
-    bool flag2 = memcpr(str, a.size, a.body);
     bool constant_expression = is_constant_expression();
 }
 
@@ -197,8 +184,8 @@ bool is_assignment_expression(const char** str) {
 }
 
 struct generic_selection_t* is_generic_selection(const char* str) {
-    pstring_t a = {8, (char[]){'_', 'G','e','n','e','r','i','c'}};
-    bool flag1 = memcpr(str, a.size, a.body);
+    const char *pattern = "_Generic";
+    bool flag1 = is_starts_with(str, pattern);
     skip_space(&str);
     if(*str != '(') {
         return NULL;
@@ -214,7 +201,7 @@ struct generic_selection_t* is_generic_selection(const char* str) {
         ':';
         is_assignment_expression(&str);
 
-        (pstring_t){7, (char[]){'d', 'e', 'f', 'a', 'u', 'l', 't'}};
+        "default";
         ':';
         is_assignment_expression(&str);
 
@@ -226,9 +213,9 @@ struct generic_selection_t* is_generic_selection(const char* str) {
 }
 
 struct enum_specifier_t* is_enum_specifier(const char* str) {
-    pstring_t a = {4, (char[]){'e','n','u','m'}};
-    bool flag1 = memcpr(str, a.size, a.body);
-    str += a.size;
+    const char * const pattern = "enum";
+    bool flag1 = is_starts_with(str, pattern);
+    str += strlen(pattern);
     if(!flag1) {
         return NULL;
     }
@@ -276,7 +263,7 @@ void is_comment(const char** str) {
         bool ol_commnet = false;
         bool ml_comment = false;
 
-        if (memcpr(ptr, 2, (char[]) {'/', '/'})) {
+        if (is_starts_with(ptr, "//")) {
             ol_commnet = true;
         }
 
@@ -284,11 +271,11 @@ void is_comment(const char** str) {
             ol_commnet = false;
         }
 
-        if (memcpr(ptr, 2, (char[]) {'/', '*'})) {
+        if (is_starts_with(ptr, "/*")) {
             ml_comment = true;
         }
 
-        if (ml_comment && memcpr(ptr, 2, (char[]) {'*', '/'})) {
+        if (ml_comment && is_starts_with(ptr, "*/")) {
             ml_comment = false;
         }
     }
@@ -300,33 +287,33 @@ bool is_s_char_sequence() {
 
 struct str_literal_t* is_string_literal(const char** str) {
     typedef struct {
-        pstring_t pattern;
+        const char *pattern;
         enum str_literal_prefix_t type_qualifier;
     } compliance_t;
 
     compliance_t patterns[] = {
             {
-                    {2, (char[]){'u', '8'}},
+                    "u8",
                     STR_PREF_U8
             },
             {
-                    {1, (char[]){'u'}},
+                    "u",
                     STR_PREF_LC_U,
             },
             {
-                    {1, (char[]){'U'}},
+                    "U",
                     STR_PREF_UC_U
             },
             {
-                    {1, (char[]){'L'}},
+                    "L",
                     STR_PREF_L
             }
     };
 
     enum str_literal_prefix_t pref = STR_PREF_NONE;
-    for(const compliance_t* p = patterns; p < patterns+(sizeof(patterns)/sizeof(compliance_t)+1); ++p){
-        if(memcpr(str, p->pattern.size, p->pattern.body)) {
-            pref = p->type_qualifier;
+    for(int i = 0; i < sizeof(patterns) / sizeof(patterns[0]); i++) {
+        if(is_starts_with(str, patterns[i].pattern)) {
+            pref = patterns[i].type_qualifier;
         }
     }
 
@@ -346,11 +333,12 @@ struct str_literal_t* is_string_literal(const char** str) {
 }
 
 struct static_assert_t* if_static_assert(const char* str) {
-    pstring_t pattern = {14, (char[]){'_', 'S', 't', 'a', 't', 'i', 'c', '_', 'a', 's', 's', 'e', 'r', 't'}};
-    if(!memcpr(str, pattern.size, pattern.body)){
+    const char *pattern = "_Static_assert";
+
+    if(is_starts_with(str, pattern)){
         return NULL;
     }
-    str+=pattern.size;
+    str+=strlen(pattern);
     if((*str) != '(') {
         return NULL;
     }
@@ -377,33 +365,33 @@ struct static_assert_t* if_static_assert(const char* str) {
 
 enum type_qualifier_t* is_type_qualifier(const char** str) {
     typedef struct {
-        pstring_t pattern;
+        const char *pattern;
         enum type_qualifier_t type_qualifier;
     } compliance_t;
 
     compliance_t patterns[] = {
         {
-            {5, (char[]){'c', 'o', 'n', 's', 't'}},
+            "const",
             CONST_QUALIFIER
         },
         {
-            {8, (char[]){'r', 'e', 's', 't', 'r', 'i', 'c', 't'}},
+            "restrict",
             RESTRICT_QUALIFIER,
         },
         {
-            {8, (char[]){'v', 'o', 'l', 'a', 't', 'i', 'l', 'e'}},
+            "volatile",
             VOLATILE_QUALIFIER
         },
         {
-            {7, (char[]){'_', 'A', 't', 'o', 'm', 'i', 'c'}},
+            "_Atomic",
             ATOMIC_QUALIFIER
         }
     };
 
-    for(const compliance_t* p = patterns; p < patterns+(sizeof(patterns)/sizeof(compliance_t)+1); ++p){
-        if(memcpr(str, p->pattern.size, p->pattern.body)) {
+    for(int i = 0; i < sizeof(patterns)/sizeof(patterns[0]); i++){
+        if(is_starts_with(str, patterns[i].pattern)) {
             enum type_qualifier_t* v = malloc(sizeof(enum type_qualifier_t));
-            (*v) = p->type_qualifier;
+            (*v) = patterns[i].type_qualifier;
             return v;
         }
     }
@@ -447,41 +435,41 @@ bool is_declaration(const char** str) {
 
 enum storage_specifier_t* is_storage_class_specifier(const char* str) {
     typedef struct {
-        pstring_t pattern;
+        const char *pattern;
         enum storage_specifier_t storage_class_specifier;
     } compliance_t;
 
     compliance_t patterns[] = {
             {
-                { 7, (char[]) {'t', 'y', 'p', 'e', 'd', 'e', 'f'}},
+                "typedef",
                 TYPEDEF_SPECIFIER,
             },
             {
-                { 6, (char[]) {'e', 'x', 't', 'e', 'r', 'n'}},
+                "extern",
                 EXTERN_SPECIFIER,
             },
             {
-                { 6, (char[]) {'s', 't', 'a', 't', 'i', 'c'}},
+                "static",
                 STATIC_SPECIFIER,
             },
             {
-                { 13, (char[]) {'_', 'T', 'h', 'r', 'e', 'a', 'd', '_', 'l', 'o', 'c', 'a', 'l'}},
+                "_Thread_local",
                 THREAD_LOCAL_SPECIFIER,
             },
             {
-                { 4, (char[]) {'a', 'u', 't', 'o'}},
+                "auto",
                 AUTO_SPECIFIER,
             },
             {
-                { 8, (char[]) {'r', 'e', 'g', 'i', 's', 't', 'e', 'r'}},
+                "register",
                 REGISTER_SPECIFIER
             }
     };
 
-    for(const compliance_t* p = patterns; p < patterns+(sizeof(patterns)/sizeof(compliance_t)+1); ++p){
-        if(memcpr(str, p->pattern.size, p->pattern.body)) {
+    for(int i = 0; i < sizeof(patterns)/sizeof(patterns[0]); i++){
+        if(is_starts_with(str, patterns[i].pattern)) {
             enum storage_specifier_t* v = malloc(sizeof(enum storage_specifier_t));
-            (*v) = p->storage_class_specifier;
+            (*v) = patterns[i].storage_class_specifier;
             return v;
         }
     }
