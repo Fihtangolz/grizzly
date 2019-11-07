718e08670017515c0ce476013514d52fb623bed6
Dmitry <mamhigt@mail.ru>
2019-11-07 09:20:55 +0700

    root: fix cli

--- src/cli.h
+++ src/cli.h
@@ -2,22 +2,35 @@
 #define GRIZZLY_CLI_H
 
 static struct argp_option options[] = {
-        {"verbose",  'v', 0,      0,  "Produce verbose output" },
-        {"quiet",    'q', 0,      0,  "Don't produce any output" },
-        {"silent",   's', 0,      OPTION_ALIAS },
-        {"output",   'o', "FILE", 0,
-                                      "Output to FILE instead of standard output" },
+        {"ast-dump",  'd', "FILE",      0,  "Produce ast to standard output" },
         { 0 }
 };
 
-static struct argp argp = {options};
+/* Program version */
+const char *argp_program_version = "0.0.0";
 
-struct arguments
-{
-    char *args[2];                /* arg1 & arg2 */
-    int silent, verbose;
-    char *output_file;
-};
+/* Program bug address */
+const char *argp_program_bug_address = "https://github.com/Fihtangolz/grizzly/issues";
 
+static const char argp_program_desc[] = "Grizzly - C11 language compiler";
+
+struct arguments_t {
+    char *target_file;
+} arguments;
+
+static error_t parse_opt (int key, char* arg, struct argp_state* state) {
+    switch (key) {
+        case 'd':
+            arguments.target_file = arg;
+            break;
+    }
+    return 0;
+}
+
+static struct argp argp = {options, parse_opt, 0, argp_program_desc};
+
+error_t cli_parse_arg(int argc, char *argv[], struct arguments_t* args) {
+    return argp_parse(&argp, argc, argv, 0, 0, args);
+}
 
 #endif //GRIZZLY_CLI_H
--- src/main.c
+++ src/main.c
@@ -8,17 +8,12 @@
 
 
 int main(int argc, char *argv[]) {
-//    struct arguments arguments;
-//    error_t error = argp_parse(&argp, argc, argv, 0, 0, &arguments);
-//    strerror(error);
-//    printf ("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
-//            "VERBOSE = %s\nSILENT = %s\n",
-//            arguments.args[0], arguments.args[1],
-//            arguments.output_file,
-//            arguments.verbose ? "yes" : "no",
-//            arguments.silent ? "yes" : "no");
+    error_t error = cli_parse_arg(argc, argv, &arguments);
+    if(error) {
+        strerror(error);
+    }
 
-    struct translation_unit_t* tu = parse("_Static_assert(,);");
+    struct translation_unit_t* tu = parse(arguments.target_file);
     ast_dump(tu, stdin);
 
     return EXIT_SUCCESS;
